c85ac1ef7ebe805e89b59db69d8469a82ce9d9a7
Giuliano Ferrari <giucoferrari@gmail.com>
2022-12-03 13:48:24 -0300

    fix typo

--- src/main/scala/Puzzle.scala
+++ src/main/scala/Puzzle.scala
@@ -19,6 +19,7 @@ trait Puzzle[I, R] {
 
   def printSolutions(): Unit =
     val rawInput = readInput()
+    val (result1, result2) = solveProblems(rawInput)
     println(s"Day: $puzzleId")
     println(s"Problem 1: $result1")
     println(s"Problem 2: $result2")
